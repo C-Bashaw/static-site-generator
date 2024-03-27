57a5cc515dc48425af4257dcf7ccd260c5ef7e00
Cody Bashaw <148400196+C-Bashaw@users.noreply.github.com>
2024-03-27 11:11:58 -0500

    updates

--- src/textnode.py
+++ src/textnode.py
@@ -3,7 +3,12 @@ class TextNode:
         self.text = text
         self.text_type = text_type
         self.url = url
-def __eq__():
-  
-        
-def __repr__():
+    def __eq__():
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
+
+    def __repr__():
+        return f"TextNode ({self.text}, {self.text_type}, {self.url})"
